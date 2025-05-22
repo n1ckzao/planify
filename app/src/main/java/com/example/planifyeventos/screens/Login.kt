c6c5dd5b789712c269647994d4247c0b19503d0d
NICOLAS LIMA MEIRA <24122474@dsn127m1246728.cfp127aluno.local>
2025-05-22 12:24:14 -0300

    estrutura iniciada

---
+++ app/src/main/java/com/example/planifyeventos/screens/Login.kt
@@ -0,0 +1,168 @@
+package com.example.planifyeventos.screens
+
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.planifyeventos.R
+
+
+@Composable
+fun Login(modifier: Modifier = Modifier) {
+    val email = remember { mutableStateOf("") }
+    val senha = remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ){
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Card(
+                modifier = Modifier
+                    .fillMaxSize(),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                shape = RoundedCornerShape(30.dp),
+                border = BorderStroke(
+                    width = 14.dp,
+                    color = Color(0xFF008EFF)
+                )
+            ) {
+                Column (
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(40.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.logo),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                    )
+                    Column (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ){
+                        Text(
+                            text = "Email:",
+                            fontSize = 15.sp
+                        )
+                        OutlinedTextField(
+                            value = email.value,
+                            onValueChange = { email.value = it },
+                            shape = RoundedCornerShape(23.dp),
+                            modifier = Modifier
+                                .height(35.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Email,
+                                imeAction = ImeAction.Next
+                            )
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "Senha:",
+                            fontSize = 15.sp
+                        )
+                        OutlinedTextField(
+                            value = senha.value,
+                            onValueChange = { senha.value = it },
+                            shape = RoundedCornerShape(23.dp),
+                            modifier = Modifier
+                                .height(35.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password,
+                                imeAction = ImeAction.Next
+                            )
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                    }
+                    Column (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Button(
+                            onClick = {},
+                            shape = RoundedCornerShape(48.dp),
+                            modifier = Modifier
+                                .width(200.dp)
+                                .height(40.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF037EF7))
+                        ) {
+                            Text(
+                                text = "Logar",
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.ExtraBold
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Row (
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ){
+                            Text(
+                                text = "Não tem uma conta?",
+                                fontSize = 15.sp
+                            )
+                            Text(
+                                text = " Cadastre-se",
+                                fontSize = 15.sp,
+                                color = Color(0xFF037EF7)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun LoginPreview() {
+    Login()
+}
